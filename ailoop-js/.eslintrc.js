d9ff8787533a371fc4de80f18a1f631afef8095a
System Two <alexandre@55seeds.com>
2026-01-19 17:43:09 +0000

    fix: add missing ESLint configuration for TypeScript SDK

---
+++ ailoop-js/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    // Basic rules
+    'no-console': 'off',
+    'no-unused-vars': 'off', // Turn off base rule as it can report incorrect errors
+    '@typescript-eslint/no-unused-vars': ['error', {
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_'
+    }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+};
